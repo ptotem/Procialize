d4dec3385e73836f13e64c12f95baf08ac8d30b9
Ptotem Learning <info@ptotem.com>
2012-11-16 22:12:40 +0530

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProcializeApp::Application.config.secret_token = 'ebcedec2aa8a33d1b2efb9444db28ace5da614c3b9f8233a7f06a6cd08519e52a92a5b982d133d3bdfcf81878ee83d68480fe844745a7e0431eb598dd948980a'
