6cd3cff9eec4e8b936c022bc813645177c181f31
Ptotem Learning <info@ptotem.com>
2012-11-19 12:38:57 +0530

    Profile Page Design

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -4,9 +4,8 @@ class Users::ProfileController < ApplicationController
     @name=@user.name
     @photo=@user.picture
     @headline=@user.headline
-    @educations=@user.educations.values[1]
-    #.map{|t| "Class of #{t.end_date.year}, #{t.school_name}"}
-    #@current_position=@user.positions.values.select{|p| p.is_current}.map{|p| "#{p.title}, #{p.company.name}"}
-    #@past_positions=@user.positions.values.select{|p| !p.is_current}.map{|p| "#{p.title}, #{p.company.name}"}
+    @educations=@user.educations.values[1].map{|t| "Class of #{t.endDate.year}, #{t.schoolName}"}
+    @current_position=@user.positions.values[1].select{|p| p.isCurrent}.map{|p| "#{p.title}, #{p.company.name}"}[0]
+    @past_positions=@user.positions.values[1].select{|p| !p.isCurrent}.map{|p| "#{p.title}, #{p.company.name}"}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,15 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :provider, :uid, :name, :location, :industry, :picture, :token, :secret, :headline, :avatar
+  attr_accessible :provider, :uid, :name, :location, :industry, :picture, :token, :secret, :headline, :positions,:educations, :avatar
+  serialize :positions
+  serialize :educations
   has_attached_file :avatar
 
   def self.find_for_linkedin_oauth(auth, signed_in_resource=nil)
-    require "open-uri"
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(provider:auth.provider, uid:auth.uid, email:auth.info.email, password:Devise.friendly_token[0, 20], name:auth.info.name, location:auth.info.location, industry:auth.info.industry, picture:auth.info.image, token:auth.credentials.token, secret:auth.credentials.secret, headline:auth.info.headline  )
+      user = User.create(provider:auth.provider, uid:auth.uid, email:auth.info.email, password:Devise.friendly_token[0, 20], name:auth.info.name, location:auth.info.location, picture:auth.info.image, token:auth.credentials.token, secret:auth.credentials.secret, headline:auth.info.headline, industry:auth.info.industry, positions:auth.extra.raw_info.positions, educations:auth.extra.raw_info.educations  )
     end
     user
   end
