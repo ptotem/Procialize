761a7993fbd21324d5f143c87044cc04df0aee84
Nilesh Panchal <neil08.panchal@gmail.com>
2012-11-29 19:12:15 +0530

    slide on both sides

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -9,16 +9,13 @@ class EventController < ApplicationController
 
   def index
 
+
+
     @event_days = @conference.event_days
     @eves=Array.new
-    #@event_days.each do |ev|
-    #  @eves<<ev.name
-    #  #@e = @eves.map{|e| e.e }
-    #end
 
-    @events = @event_days.map{|ed| ed.events}.flatten
+    @events = @event_days.map { |ed| ed.events }.flatten
 
-    #@event_days_name =EventDay.find(:name =>params[:event_day][:name])
 
   end
 
