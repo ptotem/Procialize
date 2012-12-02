b2213024e458b6c626960cee7885f37025551100
root <root@li411-254.members.linode.com>
2012-12-02 16:05:21 +0000

    Online Change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_conference
 
   def set_current_conference
-    @conference=current_user.participants.map { |p| p.conference }[0]
+    #@conference=current_user.participants.map { |p| p.conference }[0]
   end
 end
