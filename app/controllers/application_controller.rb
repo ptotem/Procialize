d1ca9957401877ff77be0d986bd797edd765664a
Ptotem Learning <info@ptotem.com>
2012-12-04 04:22:12 +0530

    Have made some changes. Please check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_conference
 
   def set_current_conference
-    @conference=current_user.participants.map { |p| p.conference }[0] unless current_user.participants.blank?
+    @conference=current_user.participants.map { |p| p.conference }[0] unless current_user.blank?
   end
 
   def after_sign_out_path_for(resource_or_scope)
