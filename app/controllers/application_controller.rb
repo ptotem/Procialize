f59895c14caf43ea50ea04e0372e3c499b4603f7
ptotem <info@ptotem.com>
2012-12-04 18:19:34 +0530

    Finalizing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,14 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_conference
 
   def set_current_conference
-    @conference=current_user.participants.map { |p| p.conference }[0] unless current_user.blank?
+    if !current_user.blank?
+      if !current_user.participants.blank?
+        @conference=current_user.participants.map { |p| p.conference }[0]
+      else
+        Participant.create!(:conference_id => Conference.last.id,:user_id => current_user.id)
+        @conference=Conference.last
+      end
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
