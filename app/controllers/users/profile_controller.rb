ec437cce504bcf058a89670143e85b469491b9ed
Ptotem Learning <info@ptotem.com>
2012-12-05 11:14:32 +0530

    Setting Profile

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -4,13 +4,7 @@ class Users::ProfileController < ApplicationController
     @name=@user.name
     @photo=@user.picture
     @headline=@user.headline
-    @educations=@user.educations.values[1].map { |t| t.endDate.blank? ? "":"Class of #{t.endDate.year}, #{t.schoolName}" }
-    @current_position=@user.positions.values[1].select { |p| p.isCurrent }.map { |p| "#{p.title}, #{p.company.name}" }[0]
-    @past_positions=@user.positions.values[1].select { |p| !p.isCurrent }.map { |p| "#{p.title}, #{p.company.name}" }
-    if Status.find_by_user_id(@user.id).blank?
-      @user_status = "I am enjoying this event a lot"
-    else
-      @user_status = Status.find_all_by_user_id(@user.id).last
-    end
+    @educations=@user.educations.values[1].map { |t| t.endDate.blank? ? "":"Class of #{t.endDate.year}, #{t.schoolName}" }.uniq
+    @positions=@user.positions.values[1].map { |p| "#{p.title}, #{p.company.name}" }.uniq
   end
 end
