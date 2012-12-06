64111b974eaf7e796cff1473358941caf79d36c3
ptotem <info@ptotem.com>
2012-12-06 16:59:55 +0530

    Added the ability to follow

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessible :body, :name, :user_id, :conference_id
-
-
   has_many :receipients, :dependent => :destroy
+  accepts_nested_attributes_for :receipients
 
   belongs_to :user
   belongs_to :conference
