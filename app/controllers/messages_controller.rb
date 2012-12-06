36b5d2dde8c5dcfdb67b51cf7ecd45df590ae1be
Ptotem Learning <info@ptotem.com>
2012-12-07 01:17:16 +0530

    Message list

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,16 @@
 class MessagesController < ApplicationController
   def index
-    @unread = Receipient.find_all_by_user_id_and_status(current_user.id,nil).map{|r| r.message}
-    @read = Receipient.find_all_by_user_id_and_status(current_user.id,true).map{|r| r.message}
+    @unread = Receipient.find_all_by_user_id_and_status(current_user.id, nil).map { |r| r.message }
+    @read = Receipient.find_all_by_user_id_and_status(current_user.id, true).map { |r| r.message }
     @sent = Message.find_all_by_user_id(current_user.id)
   end
 
   def show
     @message = Message.find(params[:id])
-    @receipient=Receipient.find_by_user_id_and_message_id(current_user.id,@message.id)
-    @receipient.status=true
-    @receipient.save
+    if @receipient=Receipient.find_by_user_id_and_message_id(current_user.id, @message.id)
+      @receipient.status=true
+      @receipient.save
+    end
   end
 
   def new
@@ -25,7 +26,7 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.save
         params[:receipient_users].each do |uid|
-          Receipient.create!(:message_id=>@message.id, :user_id=>uid)
+          Receipient.create!(:message_id => @message.id, :user_id => uid)
         end
         format.html { redirect_to messages_path, notice: 'Message was successfully created.' }
         format.json { render json: @message, status: :created, location: @message }
