4d66df8854332a58b6040f764757b788184eb5fd
Ptotem Learning <info@ptotem.com>
2012-12-08 08:18:30 +0530

    Message Features

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,13 @@ class Message < ActiveRecord::Base
   belongs_to :conference
   has_many :receipients, :dependent => :destroy
   accepts_nested_attributes_for :receipients, :reject_if => proc { |attrs| reject = %w(user_id).all? { |a| attrs[a].blank? } }, :allow_destroy => true
+
+  validate :at_least_one_receipient
+
+  def at_least_one_receipient
+    if receipients.size < 1
+      errors.add :base, "The message must have at least one receipient"
+    end
+  end
+
 end
