44e9f6ac3b5b4245d771de296e95e4c05c2d86cd
ptotem <info@ptotem.com>
2013-09-20 23:36:45 +0530

    corcon changes

--- app/controllers/exhibitor_imports_controller.rb
+++ app/controllers/exhibitor_imports_controller.rb
@@ -15,12 +15,11 @@ class ExhibitorImportsController < ApplicationController
     Spreadsheet.client_encoding = 'UTF-8'
     book = Spreadsheet.open "#{@exhibitor_sheet.exhibitor_details.path}"
     sheet1 = book.worksheet 0
-    sheet2 = book.worksheet 1
 
 
     sheet1.each_with_index do |row, index|
       if index>0
-        @exhibitors<<Exhibitor.create!(:name=> row[0], :industry => row[1], :stall_no => row[2], :description => row[3], :brouchers => File.open(row[4]), :broucher2 => File.open(row[5]),:exhi_logo => File.open(row[6]))
+        @exhibitors<<Exhibitor.create!(:name=> row[0], :industry => row[1], :stall_no => row[2], :description => row[3], :brouchers => File.open(row[4]), :broucher2 => File.open(row[5]),:exhi_logo => File.open(row[6]),:contact_no => row[7] )
       end
     end
 
