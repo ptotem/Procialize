94a754bb10378a1968ab3d4b12f9515b5c183238
ptotem <info@ptotem.com>
2013-09-25 16:15:44 +0530

    Sep 25 changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -171,34 +171,36 @@ class HomeController < ApplicationController
     @user_recommend=Array.new
     @users.each do |i|
       @user=User.find(i)
-      if @user.interest != ""
-        @user_in=User.find_all_by_industry(@user.interest).shuffle[0..3].map { |i| i.name }
-        @user_in.delete(@user.name)
-          if @user_in.count<=1
-            @user_in<<User.all.shuffle[0..3].map { |i| i.name }
-            @user_in.uniq.delete(@user.name)
-            @user_in=@user_in.flatten
-          end
+      #if @user.interest != ""
+      #  @user_in=User.find_all_by_industry(@user.interest).shuffle[0..3].map { |i| i.name }
+      #  @user_in.delete(@user.name)
+      #    if @user_in.count<=1
+      #      @user_in<<User.all.shuffle[0..3].map { |i| i.name }
+      #      @user_in.uniq.delete(@user.name)
+      #      @user_in=@user_in.flatten
+      #    end
+      #
+      #elsif @user.interest="" and @user.industry != ""
+      #  @user_in=User.find_all_by_industry(@user.industry).shuffle[0..3].map { |i| i.name }
+      #  @user_in.delete(@user.name)
+      #    if @user_in.count<=1
+      #      @user_in<<User.all.shuffle[0..3].map { |i| i.name }
+      #      @user_in.uniq.delete(@user.name)
+      #      @user_in=@user_in.flatten
+      #    end
 
-      elsif @user.interest="" and @user.industry != ""
-        @user_in=User.find_all_by_industry(@user.industry).shuffle[0..3].map { |i| i.name }
-        @user_in.delete(@user.name)
-          if @user_in.count<=1
-            @user_in<<User.all.shuffle[0..3].map { |i| i.name }
-            @user_in.uniq.delete(@user.name)
-            @user_in=@user_in.flatten
-          end
-
-      elsif @user.interest="" and @user.industry="" and @user.location !=""
+      #elsif @user.interest="" and @user.industry="" and @user.location !=""
+      #if @user.interest="" and @user.industry="" and @user.location !=""
+      if @user.location !=""
         @user_in=User.find_all_by_location(@user.location).shuffle[0..2].map { |i| i.name }
         @user_in.delete(@user.name)
            if @user_in.count<=1
-             @user_in<<User.all.shuffle[0..3].map { |i| i.name }
-             @user_in.uniq.delete(@user.name)
-             @user_in=@user_in.flatten
+             @user_in<<User.all.shuffle[0..2].map { |i| i.name }
+             @user_in.delete(@user.name)
+             @user_in=@user_in.flatten.uniq
            end
       else
-        @user_in=User.all.shuffle[0..3].map { |i| i.name }
+        @user_in=User.all.shuffle[0..2].map { |i| i.name }.uniq
       end
       @user.recommend=@user_in.to_s.gsub(/"/, "").gsub("[", "").gsub("]", "")
       @user_recommend<<@user
