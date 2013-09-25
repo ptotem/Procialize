6543ea44ca7a5b7f12ff6561f660c530ce1cec39
ptotem <info@ptotem.com>
2013-09-25 23:19:20 +0530

    mail setup changed

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -9,16 +9,14 @@
 
 
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "railscasts.com",
-    :user_name            => "rakesh.rengaraj@gmail.com",
-    :password             => "rakesh19_april",
-    :authentication       => "plain",
-    #:user_name            => "railscasts",
-    #:password             => "secret",
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "railscasts.com",
+    :user_name => "procialize@gmail.com",
+    :password => "ptotem123",
+    :authentication => "plain",
+    #:user_name => "railscasts",
+    #:password => "secret",
     :enable_starttls_auto => true
 }
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
