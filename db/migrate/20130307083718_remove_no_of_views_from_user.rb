ed7d4a195c7da320e9dd50877c3ed7bf3f50fb2a
ptotem <info@ptotem.com>
2013-10-08 13:32:08 +0530

    procon_ficci changes

--- db/migrate/20130307083718_remove_no_of_views_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNoOfViewsFromUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :no_of_views
-  end
-
-  def down
-    add_column :users, :no_of_views, :integer
-  end
-end
