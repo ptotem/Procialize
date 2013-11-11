e230c73edd6ad6fba9aa05aabd43dca2699c4a2d
Rakesh Rengaraj <rakesh.rengaraj@gmail.com>
2013-11-11 17:26:45 +0530

    changed the user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default :from => '"CII - Team Procialize" <arijit@ptemplar.com>'
+  default :from => '"CWIEME - Team Procialize" <arijit@ptemplar.com>'
   def registration_confirmation(user)
     @invitee=user
-    mail(:to => user.email, :subject => "Start interacting with delegates and speakers of CII PHARMA SUMMIT Now!")
+    mail(:to => user.email, :subject => "Start interacting with delegates and exhibitors at CWIEME 2013 now!!")
   end
 
   #def unapproval_notice(user)
