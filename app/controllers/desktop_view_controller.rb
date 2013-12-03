d7b3d0670d75915e2e75389e336aebae5311d425
Ptotem Learning Projects <info@ptotem.com>
2013-12-03 20:17:15 +0530

    Update desktop_view_controller.rb
    contact use user added

--- app/controllers/desktop_view_controller.rb
+++ app/controllers/desktop_view_controller.rb
@@ -655,25 +655,28 @@ class DesktopViewController < ApplicationController
 
 
   #-------------start of contact_us-------------------#
-  def contact_us_new
+  def new
     @message = Message.new
     @users=[]
     @followed=Follower.find_all_by_user_id(current_user.id).map { |f| User.find(f.follower_id) }
     @users<<@followed
     @users<<(User.all - @followed)
     @user_list=@users.flatten.map { |u| [u.name.titlecase, u.id] }
-    @use=@users.flatten.select{|u| (u.email=="vineet.dagar@i2ieventsgroup.com")}.map{|u| u.name }
-
+    @use=@users.flatten.select { |u| (u.email=="miera.p@gmail.com") }.map { |u| u.name }
+    
     render :layout => "application1"
   end
 
 
-  def contact_us_create
+  # Contact Us message to the concerned person
+  def create
     @message = Message.new(params[:message])
     @message.save
-    @receipient=Receipient.create!(:user_id => 2, :message_id => @message.id)
+    @receipient=Receipient.create!(:user_id => 170, :message_id => @message.id)
     @receipient.status=nil
     @receipient.save
+
+
 
     respond_to do |format|
       if @message.save
