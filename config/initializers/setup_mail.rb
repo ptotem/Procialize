4a68c763cef3f7114dd193d97e8032fd29dc6b64
Ptotem Learning Projects <info@ptotem.com>
2013-12-16 19:27:25 +0530

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,14 +8,14 @@
 #}
 
 
+
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "procialize.net",
+    :domain => "railscasts.com",
     :user_name => "procialize@gmail.com",
     :password => "ptotem123",
     :authentication => "plain",
-    :enable_starttls_auto => true,
-    :openssl_verify_mode  => 'none'
+    :enable_starttls_auto => true
 }
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
