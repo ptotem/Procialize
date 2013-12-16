a0922fdcfdb47f0080cad21738d43834e61288a6
Ptotem Learning Projects <info@ptotem.com>
2013-12-16 19:23:45 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,9 +18,6 @@ ProcializeApp::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'procialize.net' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
